9cd5eb18ea273ae471a10983aebcfc44dc1c3ea6
Aleksandr Veselov <sanek_ves@mail.ru>
2023-03-14 20:52:52 +0300

    написаны комментарии для функций

--- game_logics.py
+++ game_logics.py
@@ -1,7 +1,7 @@
 import random
 
 
-def pretty_print(massiv: list[list]) -> None:
+def pretty_print(massiv: list[list[int]]) -> None:
     """Построчный вывод массива"""
     print('-' * 10)
     for row in massiv:
@@ -9,9 +9,15 @@ def pretty_print(massiv: list[list]) -> None:
     print('-' * 10)
 
 
-def get_empty_list(massiv):
-    """Возвращает индексы массива, значения в которых равны 0"""
-    empty = []
+def get_empty_list(massiv: list[list[int]]) -> list[int]:
+    """
+    Возвращает номера клеток массива, значения в которых равны 0
+    :param massiv: массив игрового поля
+    :return: список свободных ячеек
+    """
+
+    empty = []  # список клеток
+
     for i in range(4):
         for j in range(4):
             if massiv[i][j] == 0:
@@ -20,20 +26,35 @@ def get_empty_list(massiv):
     return empty
 
 
-def get_number_from_index(i, j):
-    """Возвращает порядковый номер элемента по индексу"""
+def get_number_from_index(i: int, j: int) -> int:
+    """
+    Возвращает порядковый номер элемента по индексу
+    :param i: номер столбца
+    :param j: номер строки
+    :return: порядковый номер
+    """
     return i * 4 + j + 1
 
 
-def get_index_from_number(num):
-    """Рассчитывает индекс элемента по его порядковому номеру"""
+def get_index_from_number(num: int) -> tuple:
+    """
+    Рассчитывает индекс элемента по его порядковому номеру
+    :param num: порядковый номер ячейки
+    :return: кортеж (номер строки, номер столбца)
+    """
     num -= 1
     x, y = num // 4, num % 4
     return x, y
 
 
-def insert_2_or_4(massiv, x, y):
-    """Вставляет 2 или 4 в место c координатами x, y в массиве"""
+def insert_2_or_4(massiv: list[list[int]], x: int, y: int):
+    """
+    Вставляет 2 или 4 в место c координатами x, y в массиве
+    :param massiv: массив игрового поля
+    :param x: номер строки
+    :param y: номер столбца
+    :return: массив игрового поля с вставленными в него числами 2 или 4
+    """
     if random.random() <= 0.75:
         massiv[x][y] = 2
     else:
@@ -44,7 +65,7 @@ def insert_2_or_4(massiv, x, y):
 def is_zero_in_mas(mas: list[list]) -> bool:
     """
     Проверяет есть ли в массиве свободные места (равные 0)
-    :param mas: массив с числами
+    :param mas: массив игрового поля
     :return: True/False
     """
     for row in mas:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from game_logics import *
 
-
+# Игровое поле
 mas = [[0, 0, 0, 0],
        [0, 0, 0, 0],
        [0, 0, 0, 0],
@@ -8,10 +8,10 @@ mas = [[0, 0, 0, 0],
 
 while is_zero_in_mas(mas):
     input()
-    empty = get_empty_list(mas)
-    random.shuffle(empty)
-    random_num = empty.pop()
-    i, j = get_index_from_number(random_num)
+    empty = get_empty_list(mas)  # Список номеров клеток игрового поля со значением 0
+    random.shuffle(empty)  # Перемешивание списка
+    random_num = empty.pop()  # Удаление из списка номера пустой клетки и запись его в переменную
+    i, j = get_index_from_number(random_num)  # получение координат из номера клетки
     print(i, j)
-    mas = insert_2_or_4(mas, i, j)
+    mas = insert_2_or_4(mas, i, j)  # Вставка по полученным координатам случайного числа (2 или 4)
     print(mas)
